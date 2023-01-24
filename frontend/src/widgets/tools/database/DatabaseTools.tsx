91b7a2799c80d12df067bd85367cfe588577ea6a
Bogdan Madzhuga <43291305+bogdanspbm@users.noreply.github.com>
2023-01-24 21:08:32 +0300

    Update DatabaseTools.tsx

--- frontend/src/widgets/tools/database/DatabaseTools.tsx
+++ frontend/src/widgets/tools/database/DatabaseTools.tsx
@@ -1,7 +1,7 @@
 import styles from './DatabaseTools.module.css';
 import {Button, Input} from 'antd';
 import Title from "antd/lib/typography/Title";
-import {getDatabaseConfig} from "../../../structures/database/Database";
+import {getDatabaseConfig, setIP, setPassword, setPort, setUser} from "../../../structures/database/Database";
 import {useAppDispatch, useAppSelector} from "../../../app/hooks";
 
 
@@ -27,16 +27,16 @@ export function DatabaseTools() {
         <div className={styles.panel}>
             <Title level={5} style={{height: 16, marginLeft: 2}}>Connection</Title>
             <Input size={"large"} placeholder={"IP"} defaultValue={"localhost"} onChange={(e) => {
-                connection.ip = e.target.value;
+                dispatch(setIP({ip: e.target.value}))
             }}></Input>
             <Input size={"large"} placeholder={"Port"} defaultValue={"55000"} onChange={(e) => {
-                connection.port = e.target.value;
+                dispatch(setPort({port: e.target.value}))
             }}></Input>
             <Input size={"large"} placeholder={"User"} defaultValue={"postgres"} onChange={(e) => {
-                connection.user = e.target.value;
+                dispatch(setUser({user: e.target.value}))
             }}></Input>
             <Input size={"large"} placeholder={"Password"} defaultValue={"postgrespw"} onChange={(e) => {
-                connection.password = e.target.value;
+                dispatch(setPassword({password: e.target.value}))
             }}></Input>
             <Button size={"large"} style={{width: "100%"}} onClick={createDatabaseConnection}>Connect</Button>
         </div>)
