6121405af92199eac1f3f37e97a87014e5b7dc87
Bogdan Madzhuga <43291305+bogdanspbm@users.noreply.github.com>
2023-01-24 20:27:53 +0300

    Redux install

---
+++ frontend/src/app/store.ts
@@ -0,0 +1,19 @@
+import {Action, combineReducers, configureStore, ThunkAction,} from '@reduxjs/toolkit';
+import undoable from 'redux-undo';
+
+export const store = configureStore({
+    reducer: {
+        canvas: undoable(
+            combineReducers({})
+        ),
+    },
+});
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+    ReturnType,
+    RootState,
+    unknown,
+    Action<string>
+>;
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,15 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './app/App';
 import reportWebVitals from './report/reportWebVitals';
+import {Provider} from "react-redux";
+import {store} from "./app/store";
+
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </React.StrictMode>
 );
 
 
